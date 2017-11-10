bf7336cf0d3a1090e7a59b65cefef49ebdb39eea
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2017-11-10 15:11:38 -0300

    MULE-13670: make sure all Serializable classes have UUID

--- src/main/java/org/mule/extensions/vm/internal/ReplyToCommand.java
+++ src/main/java/org/mule/extensions/vm/internal/ReplyToCommand.java
@@ -19,6 +19,8 @@ import java.io.Serializable;
  */
 public class ReplyToCommand implements Serializable {
 
+  private static final long serialVersionUID = -6702564673951123007L;
+
   private final TypedValue<Serializable> value;
   private final String replyToQueueName;
 
