0d73e2074d6c610b904c2b543fdbed1b2f016b1a
Esteban Wasinger <estebanwasinger@gmail.com>
2018-01-22 11:46:48 -0300

    MULE-14444: VM Connector exports internal classes (#40) (#44)

--- src/main/java/org/mule/extensions/vm/api/QueueDefinition.java
+++ src/main/java/org/mule/extensions/vm/api/QueueDefinition.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.extensions.vm.internal;
+package org.mule.extensions.vm.api;
 
 import static org.mule.extensions.vm.api.QueueType.TRANSIENT;
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
